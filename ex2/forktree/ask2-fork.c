c43e8a1c4b61a5806df7508d21869899f63dba66
yannis_geo <john.k.georg@gmail.com>
2018-04-28 18:17:59 +0300

    WIP: creating process forks for ask2-fork.c (ex2.1)

--- ex2/forktree/ask2-fork.c
+++ ex2/forktree/ask2-fork.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#includei <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -26,10 +26,85 @@ void fork_procs(void)
 	sleep(SLEEP_PROC_SEC);
 
 	/* ... */
+        
+	/* Forking B process */
+	pid_t pB;
+	
+	pB = fork();
+	if (pB < 0) {
+		perror("B: fork");
+		exit(1);
+	} else if (pB == 0) {
+		mypid = getpid();
+		fork_procB();
+	}
+	// Done with B, D. On to C.
+
+	pid_t pC;
+	pC = fork();
+	if (pC<0) {
+		perror("C: fork");
+		exit(1);
+	} else if (pC == 0) {
+		mypid = getpid();
+		fork_procC();
+	}
+	/* ... */
 
 	printf("A: Exiting...\n");
 	exit(16);
 }
+
+/* proc B will fork D */
+void fork_procB(void)
+{
+	change_pname("B");
+       	printf("A: Sleeping...\n");
+      	sleep(SLEEP_PROC_SEC);
+
+       	/* ... */
+
+       	/* Forking D process */
+       	pid_t pD;
+
+       	pD = fork();
+       	if (pD < 0) {
+               	perror("D: fork");
+               	exit(1);
+       	} else if (pD == 0) {
+            	mypid = getpid();
+               	fork_procD();
+       	}
+
+        /* ... */
+
+        printf("B: Exiting...\n");
+        exit(19);
+}
+
+/* proc C (leaf) shall sleep for a while, then return */
+void fork_procC(void)
+{
+        change_pname("C");
+        printf("C: Sleeping...\n");
+        sleep(SLEEP_PROC_SEC);
+
+        printf("C: Exiting...\n");
+        exit(17);
+}
+
+
+/* proc D (leaf) shall sleep for a while, then return */
+void fork_procD(void)
+{
+        change_pname("D");
+        printf("D: Sleeping...\n");
+        sleep(SLEEP_PROC_SEC);
+
+        printf("D: Exiting...\n");
+        exit(13);
+}
+
 
 /*
  * The initial process forks the root of the process tree,
